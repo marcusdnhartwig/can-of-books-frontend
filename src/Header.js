7efca1e0b1140e95ad7cd7864fe9f3936d4649da
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-03-29 15:21:12 -0700

    header should be ready

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import LogoutButton from './LogoutButton';
 import './Header.css';
 
 class Header extends React.Component {
@@ -10,7 +11,9 @@ class Header extends React.Component {
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
         {/* TODO: if the user is logged in, render a navigation link to profile page */}
+        <NavItem><Link to='/Profile.js' className='nav-link'>Profile</Link></NavItem>
         {/* TODO: if the user is logged in, render the `LogoutButton` */}
+        <NavItem><LogoutButton onlogout={this.props.onlogout}/></NavItem>
       </Navbar>
     )
   }
--- src/Profile.js
+++ src/Profile.js
@@ -5,7 +5,8 @@ class Profile extends Component {
   render() {
     /* TODO: render information about logged in user */
     /* STRETCH TODO: if no logged in user then redirect home */
-    return <p>Profile page coming soon</p>
+    return 
+    // <p>Profile page coming soon</p>
   }
 };
 
