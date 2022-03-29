296b8545bf2237244e8beec57ea1d4dd8aa31818
Christopher Yamas <yamas.chris@gmail.com>
2022-03-29 18:59:25 -0500

    component build

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,10 +1,47 @@
-import { Component } from 'react'
+import { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import LoginForm from './LoginForm';
+import Modal from 'react-bootstrap/Modal';
+
 
 export default class LoginButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      showModal:false
+    };
+  };
+
+  modalHandler=() => {
+    this.setState({
+      showModal:true
+    });
+  };
+
 
   render() {
 
     /* TODO: Render a button with label 'Log In'. When the button is clicked then show LoginForm instead */
-    return <p>LoginButton coming soon</p>
-  }
-}
+    return (
+
+      <>
+        <Button
+        onClick={this.modalHandler}
+        >
+          User Login/Signup
+        </Button>
+
+        <Modal
+        show={this.state.showModal}
+        >
+          <Modal.Header closebutton>
+            <Modal.Title>Sign Up / Log in</Modal.Title>
+          </Modal.Header>
+          <LoginForm
+          handleLogin={this.props.handleLogin}
+          />
+        </Modal>
+      </>
+    );
+  };
+};
